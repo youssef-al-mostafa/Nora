15692cd282094e7716d189a71db69b3b85f2d808
youssef al mostafa <youssef.m@codendot.com>
2025-05-15 16:57:57 +0300

    ...

--- routes/admin.php
+++ routes/admin.php
@@ -5,11 +5,11 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::middleware(['auth:sanctum'])->group(function () {
-    Route::post('/admin/add', [UserController::class, 'addAdmin'])
-        ->name('admin.add');
-    Route::get('/admin/all', [UserController::class, 'getAdmins']);
-    Route::get('/admin/create', [UserController::class, 'createAdmin'])
-        ->name('admin.create');
+    Route::post('/admin/add', [UserController::class, 'addAdmin'])->name('admin.add');
+    Route::delete('/admin/delete', [UserController::class, 'deleteAdmin'])->name('admin.delete');
+    Route::get('/admin/all', [UserController::class, 'getAdmins'])->name('admin.all');
+    Route::get('/admin/create', [UserController::class, 'createAdmin'])->name('admin.create');
+    Route::put('/admin/changePassword', [UserController::class, 'changePasswordAdmin'])->name('admin.changePassword');
 });
 
 Route::middleware(['auth', 'verified'])->group(function () {
